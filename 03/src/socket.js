8368656b5ae6d480465f7b5311df20726b05bd14
sml782 <sml782@163.com>
2020-06-18 23:30:35 +0800

    feat: 添加 mysql 镜像’

--- 03/src/socket.js
+++ 03/src/socket.js
@@ -2,13 +2,13 @@ const net = require('net');
 const chatServer = net.createServer();
 const clientList = [];
 chatServer.on('connection',client => {
-  client.write('Hi!\n')
-  clientList.push(client)
+  client.write('Hi!\n');
+  clientList.push(client);
   client.on('data',data => {
-    console.log('receive:',data.toString())
+    console.log('receive:',data.toString());
     clientList.forEach(v => {
-      v.write(data)
+      v.write(data);
     })
   });
-})
+});
 chatServer.listen(9000);
